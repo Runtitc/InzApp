d10165f6e3df87b1f0502d78e6e742f82b592ae4
Przemo <goblinek297@gmail.com>
2016-10-08 14:07:23 -0700

    dziala na razie przelaczanie

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-public class Controller {
+public class Controller{
 
     public void registerButtonOnClick(ActionEvent event) throws IOException {
         Parent registerPage = FXMLLoader.load(getClass().getResource("view/signup.fxml"));
         Scene registerScene = new Scene(registerPage);
         Stage primaryStageRegister = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        primaryStageRegister.hide();
         primaryStageRegister.setScene(registerScene);
         primaryStageRegister.show();
     }
