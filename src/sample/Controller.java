e5d9f67c0b56a8ce41a74a6108472cfc6879b841
Runtitc <goblinek297@gmail.com>
2016-10-09 07:02:23 -0700

    przejscia pomiedzy okienkami

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,19 +6,31 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller{
+    private String registerButton = "registerButton";
+    private String registerCancel = "registerCancel";
 
-    public void registerButtonOnClick(ActionEvent event) throws IOException {
-        Parent registerPage = FXMLLoader.load(getClass().getResource("view/signup.fxml"));
+    public void onEventOccured(ActionEvent event) throws IOException {
+        System.out.println("Event " + event.getSource());
+        Button button = (Button) event.getSource();
+        String id = button.getId();
+        System.out.println("Id " + id);
+        System.out.println(registerButton);
+        if (id.equals(registerButton)){ switchScene(event, "view/signup.fxml"); }
+        if (id.equals(registerCancel)){ switchScene(event, "view/signin.fxml"); }
+    }
+
+    public void switchScene(ActionEvent event, String fxmlResource) throws IOException {
+        Parent registerPage = FXMLLoader.load(getClass().getResource(fxmlResource));
         Scene registerScene = new Scene(registerPage);
         Stage primaryStageRegister = (Stage) ((Node) event.getSource()).getScene().getWindow();
         primaryStageRegister.setScene(registerScene);
         primaryStageRegister.show();
     }
-
 }
