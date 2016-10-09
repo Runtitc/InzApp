990edecac25053f787da8878c86bc8f1a627c972
Przemo <goblinek297@gmail.com>
2016-10-09 11:06:35 -0700

    + alert

---
+++ src/sample/AlertBox.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
+/**
+ * Created by Przemix on 10/9/16.
+ */
+public class AlertBox {
+    private String typeOfAlert;
+
+    public AlertBox(String type){
+        this.typeOfAlert = type;
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Alert");
+        alert.setHeaderText("Nie wykryto zainstalowanego programu Snort");
+        alert.setContentText("Czy zainstalowac program Snort?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+        } else {
+            //System.exit(0);
+        }
+    }
+
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,19 +6,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        checkSnortInstalled(primaryStage);
+
+    }
+    public void startApp(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/signin.fxml"));
         primaryStage.setTitle("Welcome");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
+    public void checkSnortInstalled(Stage primaryStage) throws Exception {
+        String appName = "snort";
 
+        try {
+            Process snortProgram = Runtime.getRuntime().exec(appName);
+            snortProgram.destroy(); // now we destroy program cuz we will run it afterwards
+            startApp(primaryStage);
+        } catch (IOException e) {
+            System.out.println("nie ma");
+            AlertBox alert = new AlertBox("");
 
+        }
+    }
     public static void main(String[] args) {
         launch(args);
+
     }
 }
