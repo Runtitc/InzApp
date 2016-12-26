978eac35a24ab1e0ac6e4b8cffeebad836f47580
runtitc <goblinek297@gmail.com>
2016-12-26 07:27:39 -0800

    dokonczyc hashowanie hasel w bazie logika w MainWindow - laczenie z baza - laczy !!! hehehe rozdzielczosc poprawki w GUI, zrobione ip, dokonczyc warstwe tranpsortowa UDP i ICMP

---
+++ src/database/user/SnortLogICMPDetails.java
@@ -0,0 +1,51 @@
+package database.user;
+
+/**
+ * Created by runtitc on 12/25/16.
+ */
+public class SnortLogICMPDetails {
+
+    //ICMP
+    private Integer icmp_type;
+    private Integer icmp_code;
+    private Integer icmp_csum;
+    private Integer icmp_id;
+    private Integer icmp_seq;
+
+    public SnortLogICMPDetails(){}
+
+    public SnortLogICMPDetails(
+            Integer icmp_type,
+            Integer icmp_code,
+            Integer icmp_csum,
+            Integer icmp_id,
+            Integer icmp_seq
+    ){
+        this.icmp_type = icmp_type;
+        this.icmp_code = icmp_code;
+        this.icmp_csum = icmp_csum;
+        this.icmp_id= icmp_id;
+        this.icmp_seq = icmp_seq;
+    }
+
+
+    public Integer getIcmp_type() {
+        return icmp_type;
+    }
+
+    public Integer getIcmp_code() {
+        return icmp_code;
+    }
+
+    public Integer getIcmp_csum() {
+        return icmp_csum;
+    }
+
+    public Integer getIcmp_id() {
+        return icmp_id;
+    }
+
+    public Integer getIcmp_seq() {
+        return icmp_seq;
+    }
+}
---
+++ src/database/user/SnortLogUDPDetails.java
@@ -0,0 +1,44 @@
+package database.user;
+
+/**
+ * Created by runtitc on 12/25/16.
+ */
+public class SnortLogUDPDetails {
+
+    //UDP
+    private Integer udp_sport;
+    private Integer udp_dport;
+    private Integer udp_len;
+    private Integer udp_csum;
+
+    public SnortLogUDPDetails(){}
+
+    public SnortLogUDPDetails(
+            Integer udp_sport,
+            Integer udp_dport,
+            Integer udp_len,
+            Integer udp_csum
+    ){
+        this.udp_sport = udp_sport;
+        this.udp_dport = udp_dport;
+        this.udp_len = udp_len;
+        this.udp_csum= udp_csum;
+    }
+
+
+    public Integer getUdp_sport() {
+        return udp_sport;
+    }
+
+    public Integer getUdp_dport() {
+        return udp_dport;
+    }
+
+    public Integer getUdp_len() {
+        return udp_len;
+    }
+
+    public Integer getUdp_csum() {
+        return udp_csum;
+    }
+}
